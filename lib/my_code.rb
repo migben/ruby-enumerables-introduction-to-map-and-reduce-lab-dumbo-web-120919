f6e8d5454f5da06edd09808997ad4911e4bc5b89
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:20:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,39 +2,53 @@
 def map_to_negativize(source_array)
   new_arr = []
   count = 0
+  
   while count < source_array.length
     # pushing into the new arr each ele multiplied by -1 from the old arr.
-    new_arr.push( source_array[count] * -1 )
+    new_arr.push(source_array[count] * -1)
     count += 1
+    
   end
+  
   return new_arr
+  
 end
 
 def map_to_no_change(source_array)
+  
   new_arr = []
   count = 0
+  
   while count < source_array.length
-    new_arr.push( source_array[count] )
+    new_arr.push(source_array[count])
     count += 1
+    
   end
+  
   return new_arr
+  
 end
 
 def map_to_double(source_array)
   new_arr = []
   count = 0
+  
   while count < source_array.length
-    new_arr.push( source_array[count] * 2 )
+  
+    new_arr.push(source_array[count]*2)
     count += 1
+    
   end
+  
   return new_arr
+  
 end
 
 def map_to_square(source_array)
   new_arr = []
   count = 0
   while count < source_array.length
-    new_arr.push( source_array[count] * source_array[count] )
+    new_arr.push(source_array[count] * source_array[count])
     count += 1
   end
   return new_arr
