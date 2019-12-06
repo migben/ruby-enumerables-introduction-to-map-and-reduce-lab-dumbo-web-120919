159366efa3cf7b91f527183ddd0e5e0f300b3695
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:09:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,11 @@
 # My Code here....
 def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    # pushing into the new arr each ele multiplied by -1 from the old arr.
+    new.push( source_array[i] * -1 )
+    i += 1
+  end
+  return new
+end
